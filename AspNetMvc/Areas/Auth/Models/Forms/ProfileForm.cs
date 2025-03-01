69bf4e4f329fda50bbf093eb9467f5b38d62f83a
Maksym Soiko <somax2003@gmail.com>
2025-03-01 21:57:35 +0200

    Added ProfileForm class to store profile data
    Added a new class `ProfileForm` to the namespace `AspNetMvc.Areas.Auth.Models.Forms`. The class includes three properties: `ProfileImage` for loading a profile image, `FullName` for the user's full name, and `PhoneNumber` for the phone number.

---
+++ AspNetMvc/Areas/Auth/Models/Forms/ProfileForm.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetMvc.Areas.Auth.Models.Forms;
+
+public class ProfileForm
+{
+    public IFormFile? ProfileImage { get; set; }
+    public string? FullName { get; set; }
+    public string? PhoneNumber { get; set; }
+}
