4c939b1e6b8a2639638b033dd8b0c87c94b3b354
Maksym Soiko <somax2003@gmail.com>
2025-03-01 21:56:56 +0200

    Add a password change form
    Added the `AspNetMvc.Areas.Auth.Models.Forms` namespace and the `ChangePasswordForm` class with three properties: `OldPassword`, `NewPassword` and `ConfirmPassword`. This allows you to create a password change form with the required fields to enter the old and new passwords, as well as confirm the new password.

---
+++ AspNetMvc/Areas/Auth/Models/Forms/ChangePasswordForm.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetMvc.Areas.Auth.Models.Forms;
+
+public class ChangePasswordForm
+{
+    public string OldPassword { get; set; }
+    public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
+}
