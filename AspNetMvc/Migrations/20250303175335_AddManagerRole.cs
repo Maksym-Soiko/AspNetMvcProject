1ce9d018284098cb6ffa74ef65379f884e00811b
Maksym Soiko <somax2003@gmail.com>
2025-03-05 15:48:58 +0200

    Adding migration for the "Manager" role
    Added migration class `AddManagerRole`, which inserts a new role "Manager" into the `AspNetRoles` table. The `Down` method is left empty to roll back the migration.

---
+++ AspNetMvc/Migrations/20250303175335_AddManagerRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddManagerRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName" },
+                values: new object[,]
+            {
+               { Guid.NewGuid().ToString(), "Manager", "MANAGER" }
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
