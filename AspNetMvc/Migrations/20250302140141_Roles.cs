888cbc02ab5515db74fbce4268d142cbab970e7c
Maksym Soiko <somax2003@gmail.com>
2025-03-05 15:47:55 +0200

    Added migrations for roles in the database
    Migrations were added to the code that create the roles "Admin" and "User" in the table `AspNetRoles`. The `Up` method for inserting data was implemented, the `Down` method was left empty to be able to roll back the migration without deleting the data.

---
+++ AspNetMvc/Migrations/20250302140141_Roles.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class Roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName" },
+                values: new object[,]
+            {
+                { Guid.NewGuid().ToString(), "Admin", "ADMIN" },
+                { Guid.NewGuid().ToString(), "User", "USER" }
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
