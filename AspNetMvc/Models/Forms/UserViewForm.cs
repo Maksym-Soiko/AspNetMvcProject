690a6d5dd78d07ce5c112287c32490a5ec4e81fe
Maksym Soiko <somax2003@gmail.com>
2025-03-05 15:50:11 +0200

    Added UserViewForm class for managing user data
    The code implements a new `UserViewForm` class that allows displaying and editing user information. Constructors are included to initialize data from the `User` model and an `Update` method to update the model. Properties are added to store the full name, email, phone number, and profile image with appropriate validation attributes.

---
+++ AspNetMvc/Models/Forms/UserViewForm.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMvc.Models.Forms;
+
+public class UserViewForm
+{
+    public UserViewForm() { }
+
+    public UserViewForm(User model)
+    {
+        FullName = model.FullName;
+        Email = model.Email;
+        PhoneNumber = model.PhoneNumber;
+        ProfileImage = model.ProfileImage;
+    }
+
+    public void Update(User model)
+    {
+        model.FullName = FullName;
+        model.Email = Email;
+        model.PhoneNumber = PhoneNumber;
+
+        if (!string.IsNullOrEmpty(ProfileImage))
+        {
+            model.ProfileImage = ProfileImage;
+        }
+    }
+
+    [Display(Name = "Повне ім'я")]
+    [MaxLength(80, ErrorMessage = "Максимум 80 символів")]
+    public string FullName { get; set; }
+
+    [Display(Name = "Email")]
+    [EmailAddress(ErrorMessage = "Некоректна адреса електронної пошти")]
+    public string Email { get; set; }
+
+    [Display(Name = "Номер телефону")]
+    [Phone(ErrorMessage = "Некоректний номер телефону")]
+    public string PhoneNumber { get; set; }
+
+    [Display(Name = "Фото профілю")]
+    [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "Тільки файли з розширенням .jpg, .jpeg або .png дозволені.")]
+    public string? ProfileImage { get; set; }
+
+    [Required(ErrorMessage = "Виберіть хоча б одну роль!")]
+    public string[] SelectedRoles { get; set; }
+}
