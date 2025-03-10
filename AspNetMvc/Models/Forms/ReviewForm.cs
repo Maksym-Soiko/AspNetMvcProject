55bca7fc7012031160668e966d6abcd6e6d425fb
Maksym Soiko <somax2003@gmail.com>
2025-03-10 18:29:38 +0200

    Added ReviewForm class for handling reviews
    This commit adds a new `ReviewForm` class to the `AspNetMvc.Models.Forms` namespace. The class includes constructors for initialization based on the `ReviewModel` model and an `Update` method for updating model properties. Also added are `Rating`, `Text` and `UserInfoId` properties with display attributes, improving user experience.

---
+++ AspNetMvc/Models/Forms/ReviewForm.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace AspNetMvc.Models.Forms;
+
+public class ReviewForm
+{
+    public ReviewForm() { }
+
+    public ReviewForm(ReviewModel model)
+    {
+        Rating = model.Rating;
+        Text = model.Text;
+        UserInfoId = model.UserInfo.Id;
+    }
+
+    public void Update(ReviewModel model)
+    {
+        model.Rating = Rating;
+        model.Text = Text;
+        model.UserInfo = new UserInfoModel { Id = UserInfoId };
+    }
+
+    [DisplayName("Оцінка")]
+    public int Rating { get; set; }
+
+    [DisplayName("Відгук")]
+    public string Text { get; set; } = null!;
+
+    public Guid UserInfoId { get; set; }
+}
