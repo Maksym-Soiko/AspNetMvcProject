bba4cc678dd47be5ea7f165dbf54e1b40554c55e
Maksym Soiko <somax2003@gmail.com>
2025-03-10 18:30:30 +0200

    Added Reviews property to User class
    Added a new `Reviews` property to the `User` class, which is a collection of `ReviewModel` objects. This property is initialized with an empty `List<ReviewModel>()`, allowing a user to have multiple reviews.

--- AspNetMvc/Models/User.cs
+++ AspNetMvc/Models/User.cs
@@ -6,4 +6,6 @@ public class User : IdentityUser<Guid>
 {
     public string? FullName { get; set; }
     public string? ProfileImage { get; set; }
+
+    public virtual ICollection<ReviewModel> Reviews { get; set; } = new List<ReviewModel>();
 }
