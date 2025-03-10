810fc57e3418d2b9dc6cc1f725af6cab69a15d27
Maksym Soiko <somax2003@gmail.com>
2025-03-10 18:29:58 +0200

    Added ReviewModel class with basic properties
    A new class ReviewModel has been added to the ReviewModel.cs file.
    The class includes the properties Id, Text, Rating, CreatedAt, and two virtual properties UserInfo and User.
    The constructor initializes CreatedAt with the current date and time, and the rest of the properties have a default value set to null!.

---
+++ AspNetMvc/Models/ReviewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMvc.Models;
+
+public class ReviewModel
+{
+    public ReviewModel()
+    {
+        CreatedAt = DateTime.Now;
+    }
+
+    public Guid Id { get; set; }
+
+    [MaxLength(500)]
+    public string Text { get; set; } = null!;
+
+    public int Rating { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual UserInfoModel UserInfo { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
