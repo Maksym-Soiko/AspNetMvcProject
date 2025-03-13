26f15c98819486294d736a73a8599cffbb8c06a6
Maksym Soiko <somax2003@gmail.com>
2025-03-13 11:55:01 +0200

    Add migration for user geolocation
    Added migration class `UserInfoGeolocation`, which adds two new columns `Lat` and `Lng` to the `UserInfos` table. The columns are of type `real` and allow `null` values. Implemented methods `Up` and `Down` for applying and canceling migration.

---
+++ AspNetMvc/Migrations/20250310183444_UserInfoGeolocation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInfoGeolocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Lat",
+                table: "UserInfos",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Lng",
+                table: "UserInfos",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lat",
+                table: "UserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "Lng",
+                table: "UserInfos");
+        }
+    }
+}
