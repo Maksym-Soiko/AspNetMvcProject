c58ced252fbf0a0f557f04e50009215284fc5964
Maksym Soiko <somax2003@gmail.com>
2025-03-13 11:54:42 +0200

    Added user filtering method to HomeController
    A new method `GetFilteredUsers` has been added to the `HomeController` class, which handles HTTP GET requests. This method allows you to filter users by name, profession, and skill. It uses LINQ to build a database query, including related data about user reviews and skills. The result is returned in JSON format, containing information about users, including their coordinates, name, profession, and average rating.

--- AspNetMvc/Controllers/HomeController.cs
+++ AspNetMvc/Controllers/HomeController.cs
@@ -63,6 +63,42 @@ public class HomeController(
         return View("Index", users.ToList());
     }
 
+    [HttpGet]
+    public IActionResult GetFilteredUsers(string text, string[] professions, string[] skills)
+    {
+        var users = context.UserInfos
+            .Include(x => x.Reviews)
+            .Include(x => x.UserSkills)
+            .ThenInclude(x => x.Skill)
+            .AsQueryable();
+
+        if (!string.IsNullOrEmpty(text))
+        {
+            users = users.Where(u => u.Name.Contains(text));
+        }
+
+        if (professions != null && professions.Length > 0)
+        {
+            users = users.Where(u => professions.Contains(u.Profession));
+        }
+
+        if (skills != null && skills.Length > 0)
+        {
+            users = users.Where(u => skills.All(skill => u.UserSkills.Any(us => us.Skill.Title == skill)));
+        }
+
+        var result = users.Select(u => new
+        {
+            lat = u.Lat,
+            lng = u.Lng,
+            name = u.Name,
+            profession = u.Profession,
+            averageRating = u.Reviews.Any() ? u.Reviews.Average(r => r.Rating) : 0.0
+        }).ToList();
+
+        return Json(result);
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
