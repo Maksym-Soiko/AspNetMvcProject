b37cb667b76b323b7a50b5ebaf5a5e3b20f47b21
Maksym Soiko <somax2003@gmail.com>
2025-03-13 11:58:48 +0200

    Add functionality for working with a map and markers
    Added functions for processing clicks on the map, creating a marker with click coordinates. Implemented map initialization with setting the initial position depending on the entered coordinates or by default in Kyiv. The code includes creating a map, adding a tile layer and processing click events.

---
+++ AspNetMvc/wwwroot/js/map-edit.js
@@ -0,0 +1,31 @@
+﻿let marker
+let map
+
+function onMapClick(e) {
+    document.querySelector("#latitude").value = e.latlng.lat
+    document.querySelector("#longitude").value = e.latlng.lng
+    if (marker) {
+        map.removeLayer(marker)
+    }
+    marker = L.marker(e.latlng).addTo(map)
+}
+
+function initMap() {
+    let lat = document.querySelector("#latitude").value
+    let lng = document.querySelector("#longitude").value
+
+    map = L.map('map');
+
+    if (lat && lng) {
+        map.setView([parseFloat(lat), parseFloat(lng)], 12);
+        marker = L.marker([parseFloat(lat), parseFloat(lng)]).addTo(map);
+    } else {
+        map.setView([50.4501, 30.5234], 12);
+    }
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    map.on('click', onMapClick);
+}
+
+initMap();
